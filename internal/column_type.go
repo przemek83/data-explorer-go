2c928b744ba53c5bebf7f496ec586fd82bf177a7
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-11-21 19:19:22 +0100

    Create column_type.go
    Added ColumnType enum.

---
+++ internal/column_type.go
@@ -0,0 +1,17 @@
+package internal
+
+// ColumnType - Column type.
+type ColumnType int
+
+const (
+	// Integer - Integer column type.
+	Integer ColumnType = iota
+	// String - String column type.
+	String
+	// Unknown - Unknown column type.
+	Unknown
+)
+
+func (d ColumnType) String() string {
+	return [...]string{"integer", "string", ""}[d]
+}
