3c57734e4e7e7792eda02469af7675af128553ec
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-11-28 12:39:29 +0100

    Update file_data_loader_test.go
    Improvements in test - use hardcoded test string instead of file.

--- internal/file_data_loader_test.go
+++ internal/file_data_loader_test.go
@@ -2,18 +2,34 @@ package internal
 
 import (
 	"bufio"
-	"os"
+	"strings"
 	"testing"
 )
 
-func TestLoad(t *testing.T) {
-	file, err := os.Open("sample.txt")
-	defer file.Close()
-	if err != nil {
-		panic(err)
-	}
+const validInputString = `first_name;age;movie_name;score
+string;integer;string;integer
+tim;26;inception;8
+tim;26;pulp_fiction;8
+tamas;44;inception;7
+tamas;44;pulp_fiction;4
+dave;0;inception;8
+dave;0;ender's_game;8`
 
-	reader := bufio.NewReader(file)
+const emptyDataInput = ``
+
+const inputWithWrongColumnTypeName = `bla;bla;bla;bla
+string;integer;bla;integer
+tim;26;inception;8`
+
+const inputWithWrongColumnCount = `bla;bla;bla;bla
+string;integer;integer
+tim;26;inception;8`
+
+const inputWithoutData = `bla;bla;bla
+string;integer;integer`
+
+func TestLoadValidFile(t *testing.T) {
+	reader := bufio.NewReader(strings.NewReader(validInputString))
 	var loader DataLoader = FileDataLoader{}
 	ok := loader.Load(reader)
 	if !ok {
