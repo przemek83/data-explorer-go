926b3e5722c7b0df8f977bb823841baba94835df
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-11-30 20:56:19 +0100

    Update column_type.go
    Implementation for ColumnTypeFromString().

--- internal/column_type.go
+++ internal/column_type.go
@@ -16,6 +16,13 @@ func (d ColumnType) String() string {
 	return [...]string{"integer", "string", ""}[d]
 }
 
+// ColumnTypeFromString - return ColumnType for given string.
 func ColumnTypeFromString(columnTypeString string) ColumnType {
+	switch columnTypeString {
+	case Integer.String():
+		return Integer
+	case String.String():
+		return String
+	}
 	return Unknown
 }
