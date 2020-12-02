fe988e658af034fe8eb5bfd03629976224532d0a
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-12-02 19:22:43 +0100

    Update dataset.go
    Added Dataset methods with fake implementation.

--- internal/dataset.go
+++ internal/dataset.go
@@ -9,3 +9,23 @@ type Dataset struct {
 func MakeDataset(loader FileDataLoader) Dataset {
 	return Dataset{loader, []string{}, []ColumnType{}}
 }
+
+func (dataset *Dataset) initialize() bool {
+	return false
+}
+
+func (dataset *Dataset) ColumnNameToID() (bool, int) {
+	return false, -1
+}
+
+func (dataset *Dataset) ColumnIDToName() (bool, string) {
+	return false, ""
+}
+
+func (dataset *Dataset) GetColumnType() (bool, ColumnType) {
+	return false, Unknown
+}
+
+func (dataset *Dataset) GetData(columnID int) (bool, []string) {
+	return false, []string{}
+}
