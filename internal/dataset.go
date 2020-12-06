17ae53ed07cd33bd31457429bfdd499db89829ad
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-12-06 16:04:46 +0100

    Update dataset.go
    Added missing descriptions for exported symbols in dataset file.

--- internal/dataset.go
+++ internal/dataset.go
@@ -1,11 +1,13 @@
 package internal
 
+// Dataset - struct containing all loaded data with accompanying methods.
 type Dataset struct {
 	headers     []string
 	columnTypes []ColumnType
 	data        []Column
 }
 
+// MakeDataset - create dataset object.
 func MakeDataset(loader DataLoader) (bool, Dataset) {
 	if !loader.Load() {
 		return false, Dataset{}
@@ -13,6 +15,7 @@ func MakeDataset(loader DataLoader) (bool, Dataset) {
 	return true, Dataset{loader.GetHeaders(), loader.GetColumnTypes(), loader.GetData()}
 }
 
+// ColumnNameToID - convert column name given as string to id.
 func (dataset *Dataset) ColumnNameToID(name string) (bool, int) {
 	for i, currentName := range dataset.headers {
 		if currentName == name {
@@ -22,14 +25,17 @@ func (dataset *Dataset) ColumnNameToID(name string) (bool, int) {
 	return false, -1
 }
 
+// ColumnIDToName - get header name for given column index.
 func (dataset *Dataset) ColumnIDToName(id int) (bool, string) {
 	return false, ""
 }
 
+// GetColumnType - get type of column for given index.
 func (dataset *Dataset) GetColumnType(id int) (bool, ColumnType) {
 	return false, Unknown
 }
 
+// GetData - get data as a column object for given index.
 func (dataset *Dataset) GetData(id int) (bool, []Column) {
 	return false, dataset.data
 }
