a72256aaf7ec3b84dc3d967fe3f0fd9b772cbb44
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-12-06 20:45:28 +0100

    Update column_numeric_test.go
    Added TestGet().

--- internal/column_numeric_test.go
+++ internal/column_numeric_test.go
@@ -9,3 +9,13 @@ func TestGetType(t *testing.T) {
 		t.Errorf("Wrong type returned, got %v, want %v", currentType, expectedType)
 	}
 }
+
+func TestGet(t *testing.T) {
+	expectedValues := []int{1, 2, 3, 4}
+	column := ColumnNumeric{expectedValues}
+	for i, expectedValue := range expectedValues {
+		if currentValue := column.Get(i); currentValue != expectedValue {
+			t.Errorf("Wrong value returned, got %v, want %v", currentValue, expectedValue)
+		}
+	}
+}
