6865d509cdc3e29ad45dcfbb9c4fcda3d624c267
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-12-09 20:41:55 +0100

    Create operation_type.go
    Added OperationType enum.

---
+++ internal/operation_type.go
@@ -0,0 +1,28 @@
+package internal
+
+// OperationType - Operation type.
+type OperationType int
+
+const (
+	// Average - Calculate average.
+	Average OperationType = iota
+	// Minimum - Calculate minimum.
+	Minimum
+	// Maximum - Calculate maximum.
+	Maximum
+)
+
+func (d OperationType) String() string {
+	return [...]string{"avg", "min", "max"}[d]
+}
+
+// OperationTypeFromString - return OperationType for given string.
+func OperationTypeFromString(operationTypeString string) OperationType {
+	switch operationTypeString {
+	case Average.String():
+		return Average
+	case Minimum.String():
+		return Minimum
+	}
+	return Maximum
+}
