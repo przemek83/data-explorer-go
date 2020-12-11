af01bad48031e5a079515d1d225a79d79ec13895
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-12-11 22:30:10 +0100

    Update query.go
    Added MakeQuery and  ColumnsAreValid declarations.

--- internal/query.go
+++ internal/query.go
@@ -6,3 +6,13 @@ type Query struct {
 	aggregateColumnID int
 	groupingColumnID  int
 }
+
+// MakeQuery  - create query.
+func MakeQuery(args []string) (bool, Query) {
+	return false, Query{}
+}
+
+// ColumnsAreValid - check if columns on input are columns in dataset.
+func (query *Query) ColumnsAreValid(dataset *Dataset) bool {
+	return false
+}
