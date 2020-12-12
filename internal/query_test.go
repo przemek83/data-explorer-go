8c041a9eaca3227551e2d66bbc3fd8ba1b259aef
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-12-12 15:01:21 +0100

    Update query_test.go
    Extended list of test cases . Make tests run parallel.

--- internal/query_test.go
+++ internal/query_test.go
@@ -12,20 +12,72 @@ func TestMakeQuery(t *testing.T) {
 		args      []string
 		wantError bool
 		wantQuery Query
-	}{{"Proper number of args, avg operation, proper columns.", []string{"a", "b", "c"}, []string{"avg", "a", "b"}, false, Query{Average, 0, 1}}}
+	}{
+		{
+			"Proper number of args, avg operation, proper columns.",
+			[]string{"a", "b", "c"},
+			[]string{"avg", "a", "b"},
+			false,
+			Query{Average, 0, 1},
+		},
+		{
+			"Proper number of args, min operation, proper columns.",
+			[]string{"a", "b", "c"},
+			[]string{"min", "a", "b"},
+			false,
+			Query{Minimum, 0, 1},
+		},
+		{
+			"Proper number of args, max operation, proper columns.",
+			[]string{"a", "b", "c"},
+			[]string{"max", "a", "b"},
+			false,
+			Query{Maximum, 0, 1},
+		},
+		{
+			"Proper number of args, wrong operation, proper columns.",
+			[]string{"a", "b", "c"},
+			[]string{"ble", "a", "b"},
+			true,
+			Query{Average, 0, 1},
+		},
+		{
+			"Args list too long, avg operation, proper columns.",
+			[]string{"a", "b", "c", "d"},
+			[]string{"avg", "a", "b"},
+			true,
+			Query{Average, 0, 1},
+		},
+		{
+			"Args list too short, avg operation, proper columns.",
+			[]string{"a", "b"},
+			[]string{"avg", "a", "b"},
+			true,
+			Query{Average, 0, 1},
+		},
+		{
+			"Proper number of args, avg operation, wrong columns.",
+			[]string{"a", "b", "c"},
+			[]string{"avg", "a", "d"},
+			true,
+			Query{Average, 0, 1},
+		},
+	}
 
 	for _, tc := range testCases {
-		loader := newFileDataLoaderStub(tc.headers, []ColumnType{}, []Column{}, true)
-		_, dataset := MakeDataset(loader)
-		gotQuery, gotErr := MakeQuery(tc.args, &dataset)
-		if tc.wantError && gotErr == nil {
-			t.Errorf("Error unexpectedly not raised.")
-		}
-		if !tc.wantError && gotErr != nil {
-			t.Errorf("Error unexpectedly raised.")
-		}
-		if !reflect.DeepEqual(gotQuery, tc.wantQuery) {
-			t.Errorf("Queries not equal. Expected %v, got %v.", tc.wantQuery, gotQuery)
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			loader := newFileDataLoaderStub(tc.headers, []ColumnType{}, []Column{}, true)
+			_, dataset := MakeDataset(loader)
+			gotQuery, gotErr := MakeQuery(tc.args, &dataset)
+			if tc.wantError && gotErr == nil {
+				t.Errorf("Error unexpectedly not raised.")
+			}
+			if !tc.wantError && gotErr != nil {
+				t.Errorf("Error unexpectedly raised.")
+			}
+			if gotErr == nil && !reflect.DeepEqual(gotQuery, tc.wantQuery) {
+				t.Errorf("Queries not equal. Expected %v, got %v.", tc.wantQuery, gotQuery)
+			}
+		})
 	}
 }
