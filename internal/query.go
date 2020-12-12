e12695d530c99ae3294b211b185bec140ae11fb5
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-12-12 14:09:34 +0100

    Update query.go
    Change return types from bool to error.

--- internal/query.go
+++ internal/query.go
@@ -1,5 +1,7 @@
 package internal
 
+import "errors"
+
 // Query - query to be execututed.
 type Query struct {
 	operation         Operation
@@ -8,11 +10,11 @@ type Query struct {
 }
 
 // MakeQuery  - create query.
-func MakeQuery(args []string) (bool, Query) {
-	return false, Query{}
+func MakeQuery(args []string) (Query, error) {
+	return Query{}, errors.New("Not implemented")
 }
 
 // ColumnsAreValid - check if columns on input are columns in dataset.
-func (query *Query) ColumnsAreValid(dataset *Dataset) bool {
-	return false
+func (query *Query) ColumnsAreValid(dataset *Dataset) error {
+	return errors.New("Not implemented")
 }
