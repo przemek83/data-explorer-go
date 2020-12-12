b0e9f4477ac6185ca9b522e3447fd907fd48ab69
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-12-12 14:50:07 +0100

    Update query_test.go
    Make test more readable.

--- internal/query_test.go
+++ internal/query_test.go
@@ -7,25 +7,25 @@ import (
 
 func TestMakeQuery(t *testing.T) {
 	testCases := []struct {
-		name     string
-		headers  []string
-		args     []string
-		errOccur bool
-		query    Query
+		name      string
+		headers   []string
+		args      []string
+		wantError bool
+		wantQuery Query
 	}{{"Proper number of args, avg operation, proper columns.", []string{"a", "b", "c"}, []string{"avg", "a", "b"}, false, Query{Average, 0, 1}}}
 
 	for _, tc := range testCases {
 		loader := newFileDataLoaderStub(tc.headers, []ColumnType{}, []Column{}, true)
 		_, dataset := MakeDataset(loader)
-		query, err := MakeQuery(tc.args, &dataset)
-		if tc.errOccur && err == nil {
+		gotQuery, gotErr := MakeQuery(tc.args, &dataset)
+		if tc.wantError && gotErr == nil {
 			t.Errorf("Error unexpectedly not raised.")
 		}
-		if !tc.errOccur && err != nil {
+		if !tc.wantError && gotErr != nil {
 			t.Errorf("Error unexpectedly raised.")
 		}
-		if !reflect.DeepEqual(query, tc.query) {
-			t.Errorf("Queries not equal. Expected %v, got %v.", tc.query, query)
+		if !reflect.DeepEqual(gotQuery, tc.wantQuery) {
+			t.Errorf("Queries not equal. Expected %v, got %v.", tc.wantQuery, gotQuery)
 		}
 	}
 }
