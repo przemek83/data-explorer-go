aecfc42134480fcae3096d18e52436eec07d0faa
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-12-13 16:21:46 +0100

    Update calculator.go
    Check if column types matches expected ones.

--- internal/calculator.go
+++ internal/calculator.go
@@ -17,16 +17,27 @@ func MakeCalculator(dataset Dataset) Calculator {
 
 // Execute - Execute given query on dataset.
 func (calculator *Calculator) Execute(query Query) (map[string]float32, error) {
+	ok, err := calculator.columnTypesValid(query)
+	if !ok {
+		errorString := fmt.Sprintf("Grouping column with id %d not found", query.groupingColumnID)
+		return map[string]float32{}, errors.New(errorString)
+	}
+	if err != nil {
+		return map[string]float32{}, err
+	}
+
 	ok, aggregationColumn := calculator.dataset.GetData(query.aggregateColumnID)
 	if !ok {
 		errorString := fmt.Sprintf("Aggregate column with id %d not found", query.aggregateColumnID)
 		return map[string]float32{}, errors.New(errorString)
 	}
+
 	ok, groupingColumn := calculator.dataset.GetData(query.groupingColumnID)
 	if !ok {
 		errorString := fmt.Sprintf("Grouping column with id %d not found", query.groupingColumnID)
 		return map[string]float32{}, errors.New(errorString)
 	}
+
 	switch query.operation {
 	case Average:
 		return calculator.computeAvg(aggregationColumn, groupingColumn), nil
@@ -38,8 +49,23 @@ func (calculator *Calculator) Execute(query Query) (map[string]float32, error) {
 	return map[string]float32{}, errors.New("Operation unknown")
 }
 
+func (calculator *Calculator) columnTypesValid(query Query) (bool, error) {
+	ok, aggregationColumnType := calculator.dataset.GetColumnType(query.aggregateColumnID)
+	if !ok || aggregationColumnType != StringColumn {
+		errorString := fmt.Sprintf("Aggregate column with id %d not found or not string type", query.aggregateColumnID)
+		return false, errors.New(errorString)
+	}
+	ok, groupingColumnType := calculator.dataset.GetColumnType(query.groupingColumnID)
+	if !ok || groupingColumnType != NumericColumn {
+		errorString := fmt.Sprintf("Grouping column with id %d not found or not numeric type", query.groupingColumnID)
+		return false, errors.New(errorString)
+	}
+	return true, nil
+}
+
 func (calculator *Calculator) computeAvg(aggregationColumn Column, groupingColumn Column) map[string]float32 {
-	return map[string]float32{}
+	results := map[string]float32{}
+	return results
 }
 
 func (calculator *Calculator) computeMax(aggregationColumn Column, groupingColumn Column) map[string]float32 {
