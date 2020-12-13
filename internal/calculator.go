aca4e890dd536cb270f8143671f1acea6d0b3418
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-12-13 15:23:54 +0100

    Update calculator.go
    Added some logic to Calculator class.

--- internal/calculator.go
+++ internal/calculator.go
@@ -12,5 +12,37 @@ func MakeCalculator(dataset Dataset) Calculator {
 
 // Execute - Execute given query on dataset.
 func (calculator *Calculator) Execute(query Query) map[string]float32 {
+	switch query.operation {
+	case Average:
+		return map[string]float32{}
+	case Maximum:
+		return map[string]float32{}
+	case Minimum:
+		return map[string]float32{}
+	}
+	return map[string]float32{}
+}
+
+func (calculator *Calculator) computeAvg(aggregationColumn Column, groupingColumn Column) map[string]float32 {
+	return map[string]float32{}
+}
+
+func (calculator *Calculator) computeMax(aggregationColumn Column, groupingColumn Column) map[string]float32 {
+	greater := func(i, j int) bool {
+		return i > j
+	}
+	return calculator.computeExtreme(aggregationColumn, groupingColumn, greater)
+}
+
+func (calculator *Calculator) computeMin(aggregationColumn Column, groupingColumn Column) map[string]float32 {
+	lower := func(i, j int) bool {
+		return i < j
+	}
+	return calculator.computeExtreme(aggregationColumn, groupingColumn, lower)
+}
+
+func (calculator *Calculator) computeExtreme(aggregationColumn Column,
+	groupingColumn Column,
+	condition func(left, right int) bool) map[string]float32 {
 	return map[string]float32{}
 }
