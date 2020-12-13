55a9dd45d80cfe3331ecbd35a62a377e7b8201ec
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-12-13 18:38:43 +0100

    Update column_string_test.go
    Added missing test for ColumnString::GetSize().

--- internal/column_string_test.go
+++ internal/column_string_test.go
@@ -1,6 +1,8 @@
 package internal
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestColumnStringGetType(t *testing.T) {
 	column := ColumnString{}
@@ -27,5 +29,23 @@ func TestNumericColumnAppend(t *testing.T) {
 	column.Append(expectedValue)
 	if currentValue := column.Get(len(values)); currentValue != expectedValue {
 		t.Errorf("Wrong value returned, got %v, want %v", currentValue, expectedValue)
+	}
+}
+
+func TestColumnStringGetSize(t *testing.T) {
+	tests := []struct {
+		name   string
+		column *ColumnString
+		want   int
+	}{
+		{"Empty.", &ColumnString{}, 0},
+		{"Not empty.", &ColumnString{[]string{"a", "b", "c", "d"}}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.column.GetSize(); got != tt.want {
+				t.Errorf("ColumnString.GetSize() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
